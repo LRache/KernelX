ae4c920a18dad9e694001c6cb0fb768911a214c8
You Zi <2746324475@qq.com>
2025-12-28 17:16:05 +0800

    feat: set termios

--- usertests/stdio/key.c
+++ usertests/stdio/key.c
@@ -1,8 +1,16 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <termios.h>
 
 int main() {
+    printf("sizeof(termios) = %2lx\n", sizeof(struct termios));
+    // set raw mode
+    struct termios tio;
+    tcgetattr(STDIN_FILENO, &tio);
+    tio.c_lflag &= ~(ICANON | ECHO); // disable canonical mode and
+    tcsetattr(STDIN_FILENO, TCSANOW, &tio);
+    
     char c;
     while (1) {
         int n = read(STDIN_FILENO, &c, 1);
