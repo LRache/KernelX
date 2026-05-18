0de80849e4b8fa5c3a4c81a9edd2edecc2b3fc4a
ziyou <youzi_rache@foxmail.com>
2026-05-18 17:36:56 +0800

    format

--- src/fs/devfs/inode/loop_dev.rs
+++ src/fs/devfs/inode/loop_dev.rs
@@ -198,9 +198,7 @@ impl InodeOps for LoopInode {
                 self.bind_target_inode(target_inode)?;
                 Ok(0)
             }
-            Request::LOOP_CLR_FD => {
-                self.clear_target_inode()
-            }
+            Request::LOOP_CLR_FD => self.clear_target_inode(),
             Request::LOOP_SET_STATUS => self.set_status(arg, addrspace),
             Request::LOOP_GET_STATUS => self.get_status(arg, addrspace),
             Request::LOOP_BLKGETSIZE64 => {
