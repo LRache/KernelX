c818debe231621fa1b58885483c54189e566c250
ziyou <youzi_rache@foxmail.com>
2026-05-22 15:15:55 +0800

    build(loongarch): disable vector inst in kernel

--- scripts/loongarch64.mk
+++ scripts/loongarch64.mk
@@ -2,3 +2,6 @@
 
 RUSTFLAGS += -C no-vectorize-loops -C no-vectorize-slp
 RUSTFLAGS += -C target-feature=-lsx,-lasx
+
+CARGO_FLAGS += -Z build-std=core,alloc,compiler_builtins
+CARGO_FLAGS += -Z build-std-features=compiler-builtins-mem
