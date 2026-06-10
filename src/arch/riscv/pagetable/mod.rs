24befbe9211f2112b07e13442a6a9a84f12c83bf
ziyou <youzi_rache@foxmail.com>
2026-06-10 23:40:03 +0800

    feat(riscv): ignore mod sv39x4 when feature kvm is off

--- src/arch/riscv/pagetable/mod.rs
+++ src/arch/riscv/pagetable/mod.rs
@@ -1,7 +1,11 @@
 pub mod kernelpagetable;
 mod pagetable;
 mod pte;
-mod sv39x4;
+
 
 pub use pagetable::PageTable;
+
+#[cfg(feature = "kvm")]
+mod sv39x4;
+#[cfg(feature = "kvm")]
 pub use sv39x4::Sv39x4PageTable;
